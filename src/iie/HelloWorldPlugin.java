e30e60da28bb62c42df2bf24d8880e24e5f9e0dd
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-08-30 18:17:37 +0200

    Fix crash at plugin load

--- src/iie/HelloWorldPlugin.java
+++ src/iie/HelloWorldPlugin.java
@@ -8,20 +8,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
-
 public class HelloWorldPlugin extends JavaPlugin {
-	
+
 	public static Scoreboard board;
-	public static Objective hardcoreTimeDead;	
-	public static AbstractMap<String,String> deathMap = new HashMap<String,String>();
-	
-	public void onEnable(){
+	public static Objective hardcoreTimeDead;
+	public static AbstractMap<String, String> deathMap = new HashMap<String, String>();
+
+	public void onEnable() {
 		registerCommands();
 		getServer().getPluginManager().registerEvents(new DeathListener(this), this);
 		board = Bukkit.getServer().getScoreboardManager().getMainScoreboard();
-		hardcoreTimeDead = board.registerNewObjective("hardcoreTimeDead", "dummy");
+		if (board.getObjective("hardcoreTimeDead") == null)
+			hardcoreTimeDead = board.registerNewObjective("hardcoreTimeDead", "dummy");
 	}
-	public void registerCommands(){
+
+	public void registerCommands() {
 		getCommand("HelloWorld").setExecutor(new HelloWorld(this));
 	}
 
